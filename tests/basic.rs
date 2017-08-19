f559319ffe3ca20617febab62b71ecdec1ad1e4c
Alessio Coltellacci <lightplay8@gmail.com>
2017-08-19 02:32:44 +0200

    Add test for empty struct

--- tests/basic.rs
+++ tests/basic.rs
@@ -14,6 +14,9 @@ struct ExampleStruct {
     pub field_5: Vec<i32>,
 } 
 
+#[derive(QueryParams)]
+struct EmptyStruct {} 
+
 #[test]
 fn test_query_params_is_eql() {
     let example_struct = ExampleStruct {
@@ -24,5 +27,14 @@ fn test_query_params_is_eql() {
         field_5: vec!(4, 5, 8, 10),
     };
 
-    assert_eq!(example_struct.to_query_params(), "?field_1=4&field_2=hello&field_3=true&field_4=1000&"); 
+    assert_eq!(
+        example_struct.to_query_params(), 
+        "?field_1=4&field_2=hello&field_3=true&field_4=1000&field_5=4,5,8,10"
+    ); 
+}
+
+#[test]
+fn test_empty_struct() {
+    let empty_struct = EmptyStruct{};
+    assert_eq!(empty_struct.to_query_params(), ""); 
 }
